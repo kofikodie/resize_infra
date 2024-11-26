b448f9f2faf90bb6f21755b49f8a920d73d1c8bb
Kofi Kodie <k.kodieaddo@gmail.com>
2024-11-26 18:03:14 +0100

    remove policy

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -29,15 +29,6 @@ resource "aws_s3_bucket_policy" "primary_policy" {
   })
 }
 
-resource "aws_s3_bucket_public_access_block" "primary_public_access_block" {
-  bucket                  = aws_s3_bucket.primary.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-
 resource "aws_s3_bucket" "tmp" {
   bucket = var.tmp_name
 }
@@ -59,13 +50,4 @@ resource "aws_s3_bucket_policy" "tmp_policy" {
       },
     ],
   })
-}
-
-
-resource "aws_s3_bucket_public_access_block" "tmp_public_access_block" {
-  bucket                  = aws_s3_bucket.tmp.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
